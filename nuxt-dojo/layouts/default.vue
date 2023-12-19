5a008d50e248fe86b99d3619f22932fcfa2b8932
kasunAththanagoda <kasunAththanagodal@gmail.com>
2023-12-19 10:51:24 +0530

    tailwind

--- nuxt-dojo/layouts/default.vue
+++ nuxt-dojo/layouts/default.vue
@@ -3,7 +3,7 @@
         <header class="shadow-sm bg-white">
             <nav class="container mx-auto p-4 flex justify-between">
                 <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
-                <ul clas>
+                <ul class="flex gap-4">
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="/about">About</NuxtLink></li>
                     <li><NuxtLink to="/products">Products</NuxtLink></li>
