9c947645662bc93343564ebbcbd6f51a7b06a1da
kasunAththanagoda <kasunAththanagodal@gmail.com>
2023-12-19 11:11:06 +0530

    vid 7

--- nuxt-dojo/layouts/default.vue
+++ nuxt-dojo/layouts/default.vue
@@ -6,12 +6,12 @@
                 <ul class="flex gap-4">
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                    <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
                 </ul>
                 
             </nav>
         </header>
-        <div>
+        <div class="container mx-auto p-4">
             <slot></slot>
         </div>
     </div>
